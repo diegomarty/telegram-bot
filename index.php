877edd0338f905f77b38c6e94378acc9a4ea7522
Diego <dms509@hotmail.com>
2022-05-30 22:38:20 +0200

    ✨ Say hi to my bot!

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ require_once 'app.php';
 
 $input = file_get_contents('php://input');
 
-dd($input);
+$update = json_decode($input, true);
+
+$message = $update->message;
+
+$chat_id = $message->chat->id;
+
+$text = $message->text;
+
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$text");
 
 ?>

2a21d8043494ff4e979f9f139b139393d02163d8
Diego <dms509@hotmail.com>
2022-05-31 01:11:16 +0200

    ⚡️ Now get the last message

--- index.php
+++ index.php
@@ -9,16 +9,18 @@ $newBot = new TelegramBotClass($_ENV['BOT_TOKEN']);
 
 $botUpdates = $newBot->getUpdates();
 
-dump($botUpdates);
+//get last botUpdate
+$botUpdate = end($botUpdates);
 
-$newBot->setChatId($botUpdates[0]->message->chat->id);
-$newBot->setChatType($botUpdates[0]->message->chat->type);
-$newBot->setChatUsername($botUpdates[0]->message->chat->username);
-$newBot->setChatFirstName($botUpdates[0]->message->chat->first_name);
+
+$newBot->setChatId($botUpdates->message->chat->id);
+$newBot->setChatType($botUpdates->message->chat->type);
+$newBot->setChatUsername($botUpdates->message->chat->username);
+$newBot->setChatFirstName($botUpdates->message->chat->first_name);
 
 $newBot->setWebhook();
 
-$newBot->checkMessaje($botUpdates[0]->message->text);
+$newBot->checkMessaje($botUpdates->message->text);
 
 $newBot->deleteWebhook();
 
